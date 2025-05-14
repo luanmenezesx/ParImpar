774f2d7e1e0a1a92e09b5b74941a3b321de915ef
LuanGCMenezes <luangui2006@gmail.com>
2025-05-14 17:35:36 -0300

    Resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+double numero, resultado;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+resultado = numero % 2;
+
+if (resultado != 0){
+    Console.WriteLine($"{numero} é ímpar");
+}
+else
+{
+     Console.WriteLine($"{numero} é par");
+}
